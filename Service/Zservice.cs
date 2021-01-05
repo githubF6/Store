74cf857e612ff35fa3bb738cb1ee98b24938fec2
8bxf8 <2213489838@qq.com>
2021-01-05 11:22:44 +0800

    33

--- Service/Zservice.cs
+++ Service/Zservice.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using dao;
+using Model;
 
 namespace Service
 {
-    class Zservice
+    
+    public class Zservice
     {
+     Zdao dao = new Zdao();
+
+
+
     }
 }
--- dao/Zdao.cs
+++ dao/Zdao.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Model;
 
 namespace dao
 {
-    class Zdao
+    public class Zdao
     {
+        warehouseEntities war = new warehouseEntities();
+
+        public IQueryable qualy() {
+            var dl = from aa in war.admin  select new
+            {
+                UserName =aa.UserName,
+                Password = aa.Password,
+                zsName = aa.zsName,
+                Email = aa.Email,
+                Phone = aa.Phone,
+                CreateTime = aa.CreateTime,
+                Status = aa.Status
+            };
+            return dl;
+        }
     }
 }
