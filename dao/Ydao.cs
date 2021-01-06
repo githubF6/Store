f20011186e8741fcef6b6ebe04de8aace23b0dcb
yuhongji123 <2519674400@qq.com>
2021-01-06 10:51:06 +0800

    kkkk

--- dao/Ydao.cs
+++ dao/Ydao.cs
@@ -3,10 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Model;
 namespace dao
 {
-    class Ydao
+   public class Ydao
     {
+        public static int Count() 
+        {
+            warehouseEntities entities = new warehouseEntities();
+            return entities.rk.Count();
+        }
+
+
+        public static IQueryable GetRuKu(int pageIndex, int pageSize) 
+        {
+            warehouseEntities entities = new warehouseEntities();
+            PageList list = new PageList();
+            var obj = from p in entities.rk orderby p.ID select new 
+            {
+                ID= p.ID  ,
+                rkType = p.rkType ,
+                rkSupplier = p.rkSupplier,
+                Sum=  p.Sum ,
+                Price=    p.Price ,
+                check1= p.check1   ,
+                CreateUser= p.CreateUser  ,
+                CreateTime=  p.CreateTime.ToString() ,
+            };
+            return obj.Skip((pageIndex - 1) * pageSize).Take(pageSize) ;
+        }
     }
 }
--- storage/Controllers/YController.cs
+++ storage/Controllers/YController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Service;
 
 namespace storage.Controllers
 {
@@ -37,6 +38,15 @@ namespace storage.Controllers
         {
             //退货管理
             return PartialView();
+        }
+
+        public ActionResult  Count() 
+        {
+            return Json(Yservice.Count(), JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult GetRuKu(int page, int limit)
+        {
+            return Json(Yservice.GetRuKu(page, limit),JsonRequestBehavior.AllowGet);
         }
     }
 }
